9ec53369d20428c764215b63d8370ee9e29e8c60
YordyJimenezVe <yordyalejandro13@gmail.com>
2026-01-25 10:07:50 -0400

    UI: Force Megarys logo on mobile header and fix footer link

--- admin/src/app/pages/_layout/components/header-mobile/header-mobile.component.ts
+++ admin/src/app/pages/_layout/components/header-mobile/header-mobile.component.ts
@@ -12,7 +12,7 @@ export class HeaderMobileComponent implements OnInit, AfterViewInit {
   headerMenuSelfDisplay = true;
   headerMobileClasses = '';
   headerMobileAttributes = {};
-  constructor(private layout: LayoutService) {}
+  constructor(private layout: LayoutService) { }
 
   ngOnInit(): void {
     // build view by layout config settings
@@ -34,18 +34,6 @@ export class HeaderMobileComponent implements OnInit, AfterViewInit {
   }
 
   private getLogoUrl() {
-    const headerSelfTheme = this.layout.getProp('header.self.theme') || '';
-    const brandSelfTheme = this.layout.getProp('brand.self.theme') || '';
-    let result = 'logo-light.png';
-    if (!this.asideSelfDisplay) {
-      if (headerSelfTheme === 'light') {
-        result = 'logo-dark.png';
-      }
-    } else {
-      if (brandSelfTheme === 'light') {
-        result = 'logo-dark.png';
-      }
-    }
-    return `./assets/media/logos/${result}`;
+    return './assets/media/logos/logo-megarys.png';
   }
 }

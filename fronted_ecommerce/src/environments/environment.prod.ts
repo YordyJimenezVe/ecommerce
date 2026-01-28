e2e67cb8767937724cd690ce69b48f444fe8ce89
YordyJimenezVe <yordyalejandro13@gmail.com>
2026-01-28 09:36:45 -0400

    Fix: Standardize URL_BACKEND to remove trailing slash and fix concatenation in HomeInitial

--- fronted_ecommerce/src/environments/environment.prod.ts
+++ fronted_ecommerce/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL_BACKEND: 'https://megarys.com/api/',
+  URL_BACKEND: 'https://megarys.com/api',
   URL_SERVICIOS: 'https://megarys.com/api',
   URL_FROTEND: 'https://megarys.com',
 };

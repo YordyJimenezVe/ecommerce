68c18972599e64c730862c88d7c0c124649ae9c0
YordyJimenezVe <yordyalejandro13@gmail.com>
2026-02-15 17:20:03 -0400

    fix: add authorization header to system service reset request

--- admin/src/app/modules/system/service/system.service.ts
+++ admin/src/app/modules/system/service/system.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { URL_SERVICIOS } from 'src/app/config/config';
+import { AuthService } from '../../auth';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SystemService {
 
-    constructor(private http: HttpClient) { }
+    constructor(
+        private http: HttpClient,
+        public auth: AuthService,
+    ) { }
 
     resetDatabase() {
+        let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.auth.token });
         let URL = URL_SERVICIOS + "/admin/system/reset-data";
-        return this.http.post(URL, {});
+        return this.http.post(URL, {}, { headers: headers });
     }
 }

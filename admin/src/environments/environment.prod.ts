efe71d7d6767bd53983057450ec72912220d6adb
YordyJimenezVe <yordyalejandro13@gmail.com>
2026-02-27 15:26:55 -0400

    fix: Admin api URLs pointing to localhost in production env. Resolves browser local network prompt

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   isMockEnabled: false,
   apiUrl: 'api',
   // apiUrl: 'mysite.com/api',
-  URL_BACKEND: 'http://127.0.0.1:8000/',
-  URL_SERVICIOS: 'http://127.0.0.1:8000/api',
-  URL_FROTEND: 'http://localhost:4200',
+  URL_BACKEND: 'https://megarys.com/',
+  URL_SERVICIOS: 'https://megarys.com/api',
+  URL_FROTEND: 'https://megarys.com',
 };
